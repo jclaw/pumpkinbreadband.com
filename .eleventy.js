1b0a3542a142edeb600605cd8b758dd5c3a732d9
Jackson Clawson <jclaw3456@gmail.com>
2025-06-07 23:14:18 -0400

    add skeleton of all pages

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+// .eleventy.js  (ES module)
+import Image           from '@11ty/eleventy-img';
+import pluginBundle    from '@11ty/eleventy-plugin-bundle';
+import pluginImages    from './.eleventy.images.js';
+import pluginYouTube   from 'eleventy-plugin-youtube-embed';
+import embedVimeo      from 'eleventy-plugin-vimeo-embed';
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginBundle, { bundles: ['basecss'] });
+  eleventyConfig.addPlugin(pluginImages);
+  eleventyConfig.addPlugin(embedVimeo);
+  eleventyConfig.addPlugin(pluginYouTube, {
+    modestBranding: true,
+    lite: { responsive: true, thumbnailQuality: 'maxresdefault' }
+  });
+
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addWatchTarget('css/');
+  eleventyConfig.addWatchTarget('js/');
+
+  eleventyConfig.setServerOptions({ liveReload: false });
+
+  eleventyConfig.addCollection('projectsByPriority', (collection) => {
+    const projects = [
+      ...collection.getFilteredByGlob('projects/**/*.liquid'),
+      ...collection.getFilteredByGlob('projects/**/*.md')
+    ];
+
+    return projects
+      .map((project) => {
+        const folder = project.inputPath.split('/')[2];
+        project.imgPath = `./projects/${folder}`;
+        return project;
+      })
+      .sort((a, b) => b.data.priority - a.data.priority);
+  });
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pumpkinbreadband.com
+
+Run `npx @11ty/eleventy --serve` to start up a local development server and open http://localhost:8080/ in your web browser.
